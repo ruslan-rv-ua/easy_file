b593155384f9807960c6990443549bdcfb1a82d6
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2020-08-01 02:05:07 +0300

    initial code

--- easy_file/__init__.py
+++ easy_file/__init__.py
@@ -1,5 +1,7 @@
-"""Top-level package for Easy File."""
-
 __author__ = """Ruslan Iskov"""
 __email__ = 'ruslan.rv.ua@gmail.com'
 __version__ = '0.1.0'
+
+
+
+from .easy_file import TextFile, JSONFile, YAMLFile
